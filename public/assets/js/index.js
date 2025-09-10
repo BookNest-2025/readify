c9455d3b94386d2e2e50d0435a47ba0793c3aa16
Avishka Medagamagodage <aimedagamagodage2003@gmail.com>
2025-09-10 23:11:06 +0530

    completed index page for now and changed the color theme

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,58 +1,21 @@
-const carousel = document.querySelector(".carousel");
-const slider = document.querySelector(".carousel .slider");
-const indicator = document.querySelector(".carousel .indicators");
+const slider = document.getElementById("slider");
+const slides = document.querySelectorAll(".slide");
+const totalSlides = slides.length;
 
-const timeRunning = 600;
-const autoNext = 8000;
-let runTimeOut;
-let runAutoNext = setTimeout(() => {
-  showSlider("next");
-}, autoNext);
+let index = 0;
 
-const showSlider = (type) => {
-  const sliderItems = document.querySelectorAll(".slider .item");
-  const indicatorItems = document.querySelectorAll(".indicators .item");
-  const contentItems = document.querySelectorAll(".slider .content");
+function nextSlide() {
+  index++;
+  slider.style.transition = "transform 0.8s ease-in-out";
+  slider.style.transform = `translateX(-${index * 100}%)`;
 
-  if (type === "next") {
-    slider.appendChild(sliderItems[0]);
-    slider;
-    let positionLastItem = indicatorItems.length - 1;
-    indicator.prepend(indicatorItems[positionLastItem]);
-    carousel.classList.add("next");
-  } else {
-    let positionLastItem = sliderItems.length - 1;
-    slider.prepend(sliderItems[positionLastItem]);
-    indicator.appendChild(indicatorItems[0]);
-    carousel.classList.add("prev");
-  }
-
-  clearTimeout(runTimeOut);
-  runTimeOut = setTimeout(() => {
-    carousel.classList.remove("next");
-    carousel.classList.remove("prev");
-  }, timeRunning);
-
-  clearTimeout(runAutoNext);
-  runAutoNext = setTimeout(() => {
-    showSlider("next");
-  }, autoNext);
-};
-
-carousel.addEventListener("touchstart", (e) => {
-  startX = e.touches[0].clientX;
-});
-
-// Handle touch end
-carousel.addEventListener("touchend", (e) => {
-  endX = e.changedTouches[0].clientX;
-  handleSwipe();
-});
-
-function handleSwipe() {
-  if (startX - endX > 50) {
-    showSlider("next");
-  } else if (endX - startX > 50) {
-    showSlider("prev");
+  if (index === totalSlides - 1) {
+    setTimeout(() => {
+    slider.style.transition = "none";
+    slider.style.transform = "translateX(0)";
+    index = 0;
+    }, 800);
   }
 }
+
+  setInterval(nextSlide, 9000);
